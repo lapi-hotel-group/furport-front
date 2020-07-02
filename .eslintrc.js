b239f506ae43d2c931fbb0e2a11ffcef759a2bec
Lapi <tyoutugai.7@gmail.com>
2020-07-02 17:25:04 +0900

    add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    "google",
+    "plugin:prettier/recommended",
+    "plugin:react/recommended",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "require-jsdoc": 0,
+    "react/prop-types": 0,
+  },
+};
