7f885e21dc4ad586b5620f5448c829cb596a0acd
Lapi <tyoutugai.7@gmail.com>
2020-07-10 06:29:01 +0900

    fix tiny issues

--- src/components/Statistics/AttendCountChart.js
+++ src/components/Statistics/AttendCountChart.js
@@ -53,7 +53,7 @@ export default function AttendCountChart(props) {
         margin={{
           top: 5,
           right: 30,
-          left: 20,
+          left: 0,
           bottom: 5,
         }}
       >
