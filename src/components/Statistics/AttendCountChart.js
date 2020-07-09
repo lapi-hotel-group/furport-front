fe132fa5169eb2fb49a386a77cbde62598b4132d
Lapi <tyoutugai.7@gmail.com>
2020-07-10 04:32:15 +0900

    add simple chart

---
+++ src/components/Statistics/AttendCountChart.js
@@ -0,0 +1,82 @@
+import React from "react";
+import {
+  BarChart,
+  Bar,
+  Brush,
+  ReferenceLine,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import { useTheme } from "@material-ui/core/styles";
+
+export default function AttendCountChart(props) {
+  const theme = useTheme();
+  const filterdEvents = props.events
+    .filter((event) =>
+      props.attends
+        .map((el) => el === event.id)
+        .reduce((prev, current) => prev + current)
+    )
+    .map((event) => new Date(event.start_datetime));
+  console.log(filterdEvents);
+  const mostOld = filterdEvents.reduce((a, b) =>
+    a.getTime() < b.getTime() ? a : b
+  );
+  console.log(mostOld);
+  const mostOldYear = mostOld.getFullYear();
+  const mostOldMonth = mostOld.getMonth() + 1;
+  const nowYear = new Date().getFullYear();
+  const nowMonth = new Date().getMonth() + 1;
+  const data = [];
+
+  for (let y = mostOldYear, m = mostOldMonth; y <= nowYear; y++) {
+    for (; m <= 12 && (y < nowYear || m <= nowMonth); m++) {
+      data.push({
+        name: y + "-" + m,
+        count: filterdEvents.filter(
+          // eslint-disable-next-line
+          (el) => el.getFullYear() === y && el.getMonth() + 1 === m
+        ).length,
+      });
+    }
+    m = 1;
+  }
+  console.log(data);
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip
+          contentStyle={{
+            backgroundColor: theme.palette.background.default,
+          }}
+        />
+        <Legend
+          verticalAlign="top"
+          wrapperStyle={{
+            lineHeight: "40px",
+          }}
+        />
+        <ReferenceLine y={0} />
+        <Brush dataKey="name" height={30} stroke="#8884d8" />
+        <Bar dataKey="count" name="参加数" fill="#8884d8" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+}
