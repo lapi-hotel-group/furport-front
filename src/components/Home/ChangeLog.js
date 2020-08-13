393ec5bdd5479207f5efa5c43489ef144f623274
Shunran Sasaki <ssk@sasaki.world>
2020-08-13 21:37:28 +0900

    refactor: use interpolation for i18next

--- src/components/Home/ChangeLog.js
+++ src/components/Home/ChangeLog.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import i18n from "i18next";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
@@ -19,6 +20,7 @@ const useStyles = makeStyles((theme) => ({
 const ChangeLog = (props) => {
   const classes = useStyles();
   const { t } = useTranslation();
+  const language = i18n.language;
 
   return (
     <Paper className={classes.paper}>
@@ -31,7 +33,13 @@ const ChangeLog = (props) => {
             <ul>
               <li>
                 <Typography>
-                  {t("2020/07/29：")}
+                  {t("{{date, YYYY/MM/DD}}：", {
+                    date: new Intl.DateTimeFormat(language, {
+                      year: "numeric",
+                      month: "2-digit",
+                      day: "2-digit",
+                    }).format(new Date("2020-07-29")),
+                  })}
                   <a
                     href="https://github.com/lapi-hotel-group/furport-front/releases/tag/v0.2.0"
                     className={classes.link}
@@ -46,7 +54,13 @@ const ChangeLog = (props) => {
               <li>
                 <Typography>
                   <Typography>
-                    {t("2020/07/28：")}
+                    {t("{{date, YYYY/MM/DD}}：", {
+                      date: new Intl.DateTimeFormat(language, {
+                        year: "numeric",
+                        month: "2-digit",
+                        day: "2-digit",
+                      }).format(new Date("2020-07-28")),
+                    })}
                     <a
                       href="https://github.com/lapi-hotel-group/furport-front/releases/tag/v0.1.0"
                       className={classes.link}
@@ -60,7 +74,16 @@ const ChangeLog = (props) => {
                 </Typography>
               </li>
               <li>
-                <Typography>{t("2020/07/21： 公開")}</Typography>
+                <Typography>
+                  {t("{{date, YYYY/MM/DD}}：", {
+                    date: new Intl.DateTimeFormat(language, {
+                      year: "numeric",
+                      month: "2-digit",
+                      day: "2-digit",
+                    }).format(new Date("2020-07-21")),
+                  })}
+                  {t("公開")}
+                </Typography>
               </li>
             </ul>
           </Box>
