3f85aa6a135dc2693cbb6f7c1f027730d1d3a55c
Lapi <tyoutugai.7@gmail.com>
2020-08-15 17:43:49 +0900

    feat: get changelog from github releases

--- src/components/Home/ChangeLog.js
+++ src/components/Home/ChangeLog.js
@@ -5,6 +5,8 @@ import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import { Typography, Box } from "@material-ui/core";
 
+import releases from "../../releases.json";
+
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(3),
@@ -21,6 +23,28 @@ const ChangeLog = (props) => {
   const { t, i18n } = useTranslation();
   const language = i18n.language;
 
+  const content = releases.map((el, i) => (
+    <li key={i}>
+      <Typography>
+        {t("{{date}}：", {
+          date: new Intl.DateTimeFormat(language, {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+          }).format(new Date(el.created_at)),
+        })}
+        <a
+          href={el.url}
+          className={classes.link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {el.tag_name}
+        </a>
+        {t("リリース")}
+      </Typography>
+    </li>
+  ));
   return (
     <Paper className={classes.paper}>
       <Grid container spacing={6}>
@@ -29,62 +53,7 @@ const ChangeLog = (props) => {
             {t("更新履歴")}
           </Typography>
           <Box align="left">
-            <ul>
-              <li>
-                <Typography>
-                  {t("{{date}}：", {
-                    date: new Intl.DateTimeFormat(language, {
-                      year: "numeric",
-                      month: "2-digit",
-                      day: "2-digit",
-                    }).format(new Date("2020-07-29")),
-                  })}
-                  <a
-                    href="https://github.com/lapi-hotel-group/furport-front/releases/tag/v0.2.0"
-                    className={classes.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    v0.2.0
-                  </a>
-                  {t("リリース")}
-                </Typography>
-              </li>
-              <li>
-                <Typography>
-                  <Typography>
-                    {t("{{date}}：", {
-                      date: new Intl.DateTimeFormat(language, {
-                        year: "numeric",
-                        month: "2-digit",
-                        day: "2-digit",
-                      }).format(new Date("2020-07-28")),
-                    })}
-                    <a
-                      href="https://github.com/lapi-hotel-group/furport-front/releases/tag/v0.1.0"
-                      className={classes.link}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      v0.1.0
-                    </a>
-                    {t("リリース")}
-                  </Typography>
-                </Typography>
-              </li>
-              <li>
-                <Typography>
-                  {t("{{date}}：", {
-                    date: new Intl.DateTimeFormat(language, {
-                      year: "numeric",
-                      month: "2-digit",
-                      day: "2-digit",
-                    }).format(new Date("2020-07-21")),
-                  })}
-                  {t("公開")}
-                </Typography>
-              </li>
-            </ul>
+            <ul>{content}</ul>
           </Box>
         </Grid>
       </Grid>
